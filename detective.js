7776961491fc032294d91fa3a31b157382eecebe
Jimmy1186 <sususu089@gmail.com>
2020-06-07 19:24:04 +0800

    final check

--- detective.js
+++ detective.js
@@ -1,14 +1,14 @@
-function AllowDrop(event){
-    event.preventDefault();
-}
-function Drag(event){
-    event.dataTransfer.setData("text",event.currentTarget.id);
-}
-function Drop(event){
-    event.preventDefault();
-    var data=event.dataTransfer.getData("text");
-    event.currentTarget.appendChild(document.getElementById(data));
-}
+// function AllowDrop(event){
+//     event.preventDefault();
+// }
+// function Drag(event){
+//     event.dataTransfer.setData("text",event.currentTarget.id);
+// }
+// function Drop(event){
+//     event.preventDefault();
+//     var data=event.dataTransfer.getData("text");
+//     event.currentTarget.appendChild(document.getElementById(data));
+// }
 
 
 
